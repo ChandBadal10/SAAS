a99db79ccee98956639b70a7c2b2327f12ed282a
Badal Chand <badalchand636@gmail.com>
2026-08-21 15:33:46 +0545

    Completed the update users details from the admin side - backend

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,8 +1,12 @@
-import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 import { UpdateProfileDto } from './dto/update-profile.dto';
 import { ChangePasswordDto } from './dto/change-password.dto';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { Role } from 'src/generated/prisma/enums';
+import { UpdateUserStatusDto } from './dto/update-user-status.dto';
 
 @Controller('users')
 export class UsersController {
@@ -15,13 +19,14 @@ export class UsersController {
     async getMe(
         @Req() req: any
     ) {
-        const user = await this.usersService.getMe(req.user.userId);
+        return this.usersService.getMe(req.user.userId);
+    }
 
-        return {
-            success: true,
-            message: "Profile fetched successfully",
-            data: user
-        }
+    @Get()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN)
+    async getAllUsers() {
+    return this.usersService.getAllUsers();
     }
 
     //update profile
@@ -43,19 +48,46 @@ export class UsersController {
 
     //change Password
     @Patch('change-password')
-@UseGuards(JwtAuthGuard)
-async changePassword(
-  @Req() req: any,
-  @Body() changePasswordDto: ChangePasswordDto,
-) {
-  const result = await this.usersService.changePassword(
-    req.user.userId,
-    changePasswordDto,
-  );
+    @UseGuards(JwtAuthGuard)
+    async changePassword(
+    @Req() req: any,
+    @Body() changePasswordDto: ChangePasswordDto,
+    ) {
+    const result = await this.usersService.changePassword(
+        req.user.userId,
+        changePasswordDto,
+    );
 
-  return {
-    success: true,
-    message: result.message,
-  };
-}
+    return {
+        success: true,
+        message: result.message,
+    };
+    }
+
+    //get user by id
+    @Get(":id")
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN)
+    async getUserById(
+        @Param("id") id: string
+    ) {
+        return this.usersService.getUserById(id);
+    }
+
+
+    //Update user status
+    @Patch(":id/status")
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(Role.ADMIN)
+    async updateUserStatus(
+        @Param("id") id: string,
+        @Body() updateUserStatusDto: UpdateUserStatusDto,
+    ) {
+        const user = await this.usersService.updateUserStatus(id, updateUserStatusDto.isActive);
+
+        return {
+            success: true,
+            message: updateUserStatusDto.isActive ? "User account activated successfully" : "User account deactivated successfully", data: user
+        }
+    }
 }
