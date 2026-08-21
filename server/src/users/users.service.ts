5ffaaceee448822eaabb42cb2066b75a1c327474
Badal Chand <badalchand636@gmail.com>
2026-08-21 12:50:55 +0545

    Completed the change passwrod api also

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,10 +1,18 @@
 import {
   Injectable,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 
 import { PrismaService } from '../prisma/prisma.service';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { ChangePasswordDto } from './dto/change-password.dto';
+import * as bcrypt from 'bcrypt';
+
+
+
+
+
 
 @Injectable()
 export class UsersService {
@@ -77,5 +85,50 @@ export class UsersService {
     });
 
     return updateUser
+  }
+
+
+  //Chnage Password
+  async changePassword(userId: string, chnagePasswordDto: ChangePasswordDto) {
+    const {currentPassword, newPassword} = chnagePasswordDto;
+    //Find User
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id: userId
+      },
+    });
+
+    if(!user) {
+      throw new NotFoundException("User not found")
+    }
+
+    const isCurrentPasswordValid = await bcrypt.compare(currentPassword, user.passwordHash);
+
+    if(!isCurrentPasswordValid) {
+      throw new UnauthorizedException("Current Password is Incorrect");
+    }
+
+    const isSamePassword = await bcrypt.compare(newPassword, user.passwordHash);
+
+    if(isSamePassword) {
+      throw new UnauthorizedException("New Pasword must be different from the current password");
+    }
+
+    const newPasswordHash = await bcrypt.hash(newPassword, 12);
+
+    await this.prisma.user.update({
+      where: {
+        id: userId
+      },
+      data: {
+        passwordHash: newPasswordHash,
+        refreshTokenHash: null
+      }
+    });
+
+    return {
+      message: "Password changed successfully"
+    }
+
   }
 }
