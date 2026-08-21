738736780f818f9e1f8432fc2ab0acd94782d00c
Badal Chand <badalchand636@gmail.com>
2026-08-21 12:14:31 +0545

    Completed the Update Profile Section

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('users')
 export class UsersController {
@@ -18,6 +19,22 @@ export class UsersController {
         return {
             success: true,
             message: "Profile fetched successfully",
+            data: user
+        }
+    }
+
+    //update profile
+    @Patch("me")
+    @UseGuards(JwtAuthGuard)
+    async updateMe(
+        @Req() req: any,
+        @Body() updateProfileDto: UpdateProfileDto
+    ) {
+        const user = await this.usersService.updateMe(req.user.userId, updateProfileDto);
+
+        return {
+            success: true,
+            message: "Profile update successfully",
             data: user
         }
     }
